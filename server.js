b55ca15f3bb5e73b7ed55c01a835f7dd93902137
Keith <keithfenech123@gmail.com>
2019-02-24 15:34:23 +0100

    'Test'

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const hbs = require('hbs');
 const fs = require('fs');
+//const bootstrap = require('bootstrap');
 
 const port = process.env.PORT || 3000;
-
 var app = express();
-//hbs.registerPartials(__dirname + '/views/partials')
-//app.set('view engine', 'hbs');
-//app.use(express.static(__dirname + '/public'));
 
-var publicDir = require('path').join(__dirname,'/media');
+
+var publicDir = require('path').join(__dirname + '/public');
 app.use(express.static(publicDir));
 
 app.use((req, res, next) => {
@@ -23,17 +20,6 @@ app.use((req, res, next) => {
         }
     });
     next();
-});
-
-
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear()
-});
-
-app.get('/', (req, res) => {
-   res.render('home.hbs', {
-
-   });
 });
 
 
